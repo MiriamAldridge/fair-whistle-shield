d75498473dd90c3eb17f094c1f2fedb2909e0b9d
AdonisStephens <majnaq58240@outlook.com>
2025-11-12 22:13:29 +0000

    fix: add missing await in deployment script and correct console output

--- deploy/deployWhistleBlower.ts
+++ deploy/deployWhistleBlower.ts
@@ -5,12 +5,12 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedWhistleBlower = deploy("WhistleBlower", {
+  const deployedWhistleBlower = await deploy("WhistleBlower", {
     from: deployer,
     log: true,
   });
 
-  console.log(`WhistleBlower contract: `, deployedWhistleBlower);
+  console.log(`WhistleBlower contract: `, deployedWhistleBlower.address);
 };
 export default func;
 func.id = "deploy_whistleBlower"; // id required to prevent reexecution
